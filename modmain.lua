7aae55bcedcebc8666ed9145c85817e0550cf27e
BBGoat <bbgoat@vip.qq.com>
2025-11-10 00:17:15 +0800

    version 0.6.1 - Update the Chinese translation and move the strings used by the mod into strings.lua

--- modmain.lua
+++ modmain.lua
@@ -1,2 +1,3 @@
+modimport("scripts/strings")
 AddClassPostConstruct("widgets/redux/modstab", GLOBAL.require "widgets/remi_newmodstab")
 --GLOBAL.remi_convconf_texttest = function() print("'"..GetModConfigData("TEXT_TEST").."'") end
